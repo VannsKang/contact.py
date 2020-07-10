bf99fb526327d720369a1fe8b4640bdb26738470
Soomyung Kang <vannskang@gmail.com>
2020-07-10 16:56:54 +0900

    2nd commit

--- contact.py
+++ contact.py
@@ -1,5 +1,20 @@
+class Contact:
+    def __init__(self, name, phone_number, e_mail, addr):
+        self.name = name
+        self.phone_number = phone_number
+        self.e_mail = e_mail
+        self.addr = addr
+
+    def print_info(self):
+        print("Name: ", self.name)
+        print("Phone Number: ", self.phone_number)
+        print("E-mail: ", self.e_mail)
+        print("Address: ", self.addr)
+
+
 def run():
-    print("contact")
+    kim = Contact('ilgu kim', '010-2323-2323', '23232@gmail.com', 'Seoul')
+    kim.print_info()
 
 
 if __name__ == "__main__":
